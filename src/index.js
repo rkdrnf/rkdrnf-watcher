378f09e1cd6bd626c46e720e5eb41ac7bb8915bf
JunHyeong Ahn <rkdrnf@gmail.com>
2019-07-18 14:39:28 +0900

    umd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 const Component = require("./component");
 
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD
-        define([], factory);
-    } else if (typeof exports === 'object') {
-        // Node, CommonJS-like
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.rkdrnfWatcher = factory();
-    }
-}(this, function () {
-    return {
-        Component
-    };
-}));
+// (function (root, factory) {
+//     if (typeof define === 'function' && define.amd) {
+//         // AMD
+//         define([], factory);
+//     } else if (typeof exports === 'object') {
+//         // Node, CommonJS-like
+//         module.exports = factory();
+//     } else {
+//         console.log('aaa');
+//         // Browser globals (root is window)
+//         root.rkdrnfWatcher = factory();
+//     }
+// }(this, function () {
+//     return {
+//         Component
+//     };
+// }));
 
+
+module.exports = {
+    Component
+};
--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,11 @@ const MinifyPlugin = require("babel-minify-webpack-plugin");
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'rkdrnf-watcher.minified.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new MinifyPlugin()
-  ]
+    filename: 'rkdrnf-watcher.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'lib',
+    umdNamedDefine: true,
+    globalObject: `(typeof self !== 'undefined' ? self : this)`
+  }
 };
